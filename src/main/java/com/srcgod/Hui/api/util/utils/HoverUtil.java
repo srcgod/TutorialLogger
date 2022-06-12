d4f3e846dfff1365c02b7b0c216519ca1672fc84
sledk <srcgod>
2022-06-12 03:08:06 +0300

    fix/del

--- src/main/java/com/srcgod/Hui/api/util/utils/HoverUtil.java
+++
@@ -1,7 +0,0 @@
-package com.srcgod.Hui.api.util.utils;
-
-public class HoverUtil {
-    public static boolean hovered(int mouseX, int mouseY, int x, int y, int width, int height) {
-        return mouseX > x && mouseY > y && mouseX < width && mouseY < height;
-    }
-}
